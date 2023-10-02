c580b89646f6e29f7de31b2c25018580ac0dab36
Le Dam Quang Vinh <s3818480@rmit.edu.vn>
2023-10-02 09:07:37 +0700

    feat: ReadMe update List exercises

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This repository is used for learning Data Structure using java language.
 ### Array Data Structure
 - https://www.geeksforgeeks.org/array-data-structure/?ref=lbp
 
+### List
+- https://www.geeksforgeeks.org/data-structures/linked-list/?ref=lbp
---
+++ demo08-ldqvinh/src/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Name: Le Dam Quang Vinh
+ * Project: Rearrange positive and negative numbers in O(n) time and O(1) extra space
+ * Date: 28 Sep 2023
+ * Copy Right: Three Musketeer
+ */
+
+public class Main {
+    public static void main(String[] args) {
+       int[] a = {-1, 2, -3, 4, 5, 6, -7, 8, 9};
+       printArr(a);
+    }
+
+    public static int[] rearrange(int[] a) {
+        for(int i = 0; i < a.length;i++) {
+            
+        }
+    }
+
+    public static void printArr(int[] a) {
+        for(int i : a){
+            System.out.print(i + " ");
+        }
+    }
+}
